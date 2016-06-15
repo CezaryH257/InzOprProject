bdbee3a74c085ed1c4981be1c817a5aff99d218c
KKawski <latre123@o2.pl>
2016-06-15 18:39:21 +0200

    Update TimeTable.java

--- src/parser/TimeTable.java
+++ src/parser/TimeTable.java
@@ -1,23 +1,39 @@
 package parser;
 import java.util.ArrayList;
 
+/** Klasa reprezentująca plan lekcji w formie listy obiektów Lesson
+ * @author Krzysztof Kawski
+ *
+ */
 public class TimeTable {
 	private ArrayList <Lesson> lessonList;
 	
 	
+	/** Konstruktor tworzy nowy obiekt typu TimeTable i przypisuje
+	 * referencji lessonList nowy obiekt typu ArrayList
+	 */
 	public TimeTable() {
 		super();
 		this.lessonList = new ArrayList<>();
 	}
 
+	/** Metoda ta dodaje lekcję do listy lekcji
+	 * @param lesson - obiekt lekcji
+	 */
 	public void addLessonToList (Lesson lesson){
 		lessonList.add(lesson);
 	}
 	
+	/** Metoda ta zwraca liste lekcji
+	 * @return lessonList - lista zawierająca lekcje
+	 */
 	public ArrayList<Lesson> getLessonList() {
 		return lessonList;
 	}
 
+	/** Metoda ta ustawia  liste zajeć na te podaną w parametrze lessonList
+	 * @param lessonList - lista z zajęciami
+	 */
 	public void setLessonList(ArrayList<Lesson> lessonList) {
 		this.lessonList = lessonList;
 	}
