ef450180c85df4563c7200b29710f8b3302d5075
CezaryH257 <risotto215@gmail.com>
2016-06-16 11:29:32 +0200

    Update Pane2Controller.java

--- src/controller/Pane2Controller.java
+++ src/controller/Pane2Controller.java
@@ -7,12 +7,16 @@ import java.util.ResourceBundle;
 import application.Main;
 import application.TemporaryObject;
 import controller.LayoutController.ActionType;
+import csv.CsvGenerator;
+import iCal.Exporter;
 import iCal.VLesson;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import parser.TimeTable;
 
 public class Pane2Controller implements Initializable {
@@ -27,7 +31,9 @@ public class Pane2Controller implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		configureButtons();
 	}
-	
+	/**
+	 * Funkcja implementująca sposób zachowania przycisków.
+	 */
 	private void configureButtons(){
 		LayoutController lC = new LayoutController();
 		Button csvButton = exportPaneController.getCsvButton();
@@ -47,6 +53,27 @@ public class Pane2Controller implements Initializable {
 					vlessons.add(vlesson);
 				}
 				tempObj.setVlessonList(vlessons);
+				ExtensionFilter extType = new FileChooser.ExtensionFilter("iCalendar type", "*.ics");
+				tempObj.setExtType(extType);
+				lC.changeLayout(ActionType.NEXT);
+			}
+			
+		});
+		
+		csvButton.setOnAction(new EventHandler <ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				TemporaryObject tempObj = Main.tempObjRef;
+				TimeTable timeTable = Main.tempObjRef.getTimeTable();
+				CsvGenerator csvGenerator = new CsvGenerator();
+				
+				csvGenerator.createListWithTextLines(timeTable);
+				
+				tempObj.setCsvGenerator(csvGenerator);
+						
+				ExtensionFilter extType = new FileChooser.ExtensionFilter("CSV type", "*.csv");
+				tempObj.setExtType(extType);
 				lC.changeLayout(ActionType.NEXT);
 			}
 			
